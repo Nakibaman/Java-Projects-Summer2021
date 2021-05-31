6282394b357cecb9a6b13712d6faeb8d34af8076
Nakibaman <nakibaman@gmail.com>
2021-05-31 09:25:54 +0600

    new
    new

---
+++ Summer2021Lab3/src/summer2021lab3/Summer2021Lab3.java
@@ -0,0 +1,11 @@
+
+package summer2021lab3;
+
+public class Summer2021Lab3 {
+
+    public static void main(String[] args) {
+        float [] points={1.00f,2.00f,3.00f,4.00f,5.00f};
+        for(float i:points)
+            System.out.println(i);
+    }
+}
