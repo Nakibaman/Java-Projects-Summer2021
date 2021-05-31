e75741dae593fbd17642da7d9444e21a161a21d7
Nakibaman <nakibaman@gmail.com>
2021-05-31 11:32:46 +0600

    new7
    new7

--- Summer2021Lab3/src/summer2021lab3/Task4.java
+++ Summer2021Lab3/src/summer2021lab3/Task4.java
@@ -3,7 +3,17 @@ package summer2021lab3;
 
 public class Task4 {
     public static void main(String[] args) {
-        String fun="The quick brown fox jumps over the lazy dog";
-        System.out.println("Length is - "+fun.length());
+        String fun1="The quick brown fox jumps over the lazy dog ";
+        String fun2="The quick brown fox jumps over the lazy dog ";
+        String fun3="The quick brown fox jumps over the lazy Dog ";
+        String big_msg=fun1+fun2+fun3;
+        int words=0;
+        for(int i=0;i<big_msg.length();i++)
+        {
+            if(big_msg.charAt(i)==' ')
+               words++;
+        }
+        System.out.println("The string has "+words+" words");
+        
     }
 }
