dcb8a2f25d42d0f1e82a9ff6aaf404e42c52ff2a
Nakibaman <nakibaman@gmail.com>
2021-05-31 11:11:17 +0600

    new6
    new6

---
+++ Summer2021Lab3/src/summer2021lab3/Task4.java
@@ -0,0 +1,9 @@
+
+package summer2021lab3;
+
+public class Task4 {
+    public static void main(String[] args) {
+        String fun="The quick brown fox jumps over the lazy dog";
+        System.out.println("Length is - "+fun.length());
+    }
+}
