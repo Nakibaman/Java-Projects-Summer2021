86c50788b2efc9923473fae21660562d0d8aabf5
Nakibaman <nakibaman@gmail.com>
2021-06-07 10:33:14 +0600

    amnitei
    amnitei

---
+++ Summer2021Lab3/Summer2021Lab4/src/summer2021lab4/Amnitei2.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package summer2021lab4;
+
+/**
+ *
+ * @author Kheya
+ */
+public class Amnitei2 {
+    
+}
