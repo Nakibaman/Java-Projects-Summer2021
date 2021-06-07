f055882f408f4f2c792cbc28293c2ed1090fb159
Nakibaman <nakibaman@gmail.com>
2021-06-07 09:52:33 +0600

    task 1
    task 1

---
+++ Summer2021Lab3/Summer2021Lab4/src/summer2021lab4/Summer2021Lab4.java
@@ -0,0 +1,18 @@
+
+package summer2021lab4;
+
+
+public class Summer2021Lab4 {
+
+    public static void main(String[] args) {
+        float a[]=new float[4];
+        a[0]=23.0f;
+        a[1]=45.0f;
+        a[2]=34.0f;
+        a[3]=50.0f;
+        String msg="";
+        for(float i:a)
+            System.out.println(i);
+    }
+    
+}
