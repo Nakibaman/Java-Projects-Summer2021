322afb0a15c80ec1d90f26a89f6173d0fae5f6ec
Nakibaman <nakibaman@gmail.com>
2021-06-07 10:39:35 +0600

    update 4
    update 4

--- Summer2021Lab3/Summer2021Lab4/src/summer2021lab4/Amnitei2.java
+++ Summer2021Lab3/Summer2021Lab4/src/summer2021lab4/Amnitei2.java
@@ -1,14 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package summer2021lab4;
 
-/**
- *
- * @author Kheya
- */
+import javax.swing.JOptionPane;
+
+
 public class Amnitei2 {
-    
+    public static void main(String[] args) {
+        String reply=JOptionPane.showInputDialog("Enter array size -");
+        int array_size=Integer.parseInt(reply);
+        int array[]=new int[100];
+        for(int i=0;i<array_size;i++)
+        {
+            reply=JOptionPane.showInputDialog("Enter element on array["+i+"] -");
+            array[i]=Integer.parseInt(reply);
+        }
+        int max=array[0];
+        for(int i=1;i<array_size;i++)
+        {
+            if(array[i]>max)
+                max=array[i];
+        }
+        JOptionPane.showMessageDialog(null, "The max value is - "+max);
+                
+    }
 }
