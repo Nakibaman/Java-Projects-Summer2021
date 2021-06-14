b666c0c95fdb85694302c0b33a02f70fe19a1937
Nakibaman <nakibaman@gmail.com>
2021-06-14 09:36:55 +0600

    update 2
    update 2

---
+++ Summer2021Lab3/Summer2021Lab5/src/summer2021lab5/Summer2021Lab5.java
@@ -0,0 +1,20 @@
+
+package summer2021lab5;
+
+public class Summer2021Lab5 {
+    
+    static int udmax(int a,int b)
+    {
+        if(a>b)
+        return a;
+        else
+        return b;
+    }
+    
+    public static void main(String[] args) {   
+        
+        System.out.println(udmax(23,12));
+          
+    }
+    
+}
